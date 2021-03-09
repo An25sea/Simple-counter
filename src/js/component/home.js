9c87cd7a2478a8e5360aa9ea8248ceea0f99de68
Dilana777 <dylana7792@hotmail.com>
2021-03-09 02:48:22 +0000

    solo contador

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export default function Home() {
+	let [seconds, setSeconds] = useState(1);
 
-//create your first component
-export function Home() {
+	useEffect(() => {
+		let timer = setInterval(() => {
+			setSeconds(seconds + 1);
+		}, 1000);
+		// clearing interval
+		return () => clearInterval(timer);
+	});
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<h1>Number of seconds is {seconds}</h1>
 		</div>
 	);
 }
