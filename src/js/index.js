88ee4add30afdb23ebb3801c7d74013d56b6bbbb
Dilana777 <dylana7792@hotmail.com>
2021-03-09 16:27:04 +0000

    codigo a

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
-import Home from "./component/home.js";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Home';
+ReactDOM.render(Home />, document.getElementById('root'));
