557dddb216aa30df6a1617feb359024962c52b62
Dilana777 <dylana7792@hotmail.com>
2021-03-12 23:53:41 +0000

    ayuda de aaron

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Home';
-ReactDOM.render(Home />, document.getElementById('root'));
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+import { props } from "prop-types";
+
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import { Home } from "./component/home.js";
+
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
